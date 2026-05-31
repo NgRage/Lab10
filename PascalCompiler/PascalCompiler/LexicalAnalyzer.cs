a44b2cfeeb83424dea8fc99369b6f73b2d9bdd76
NgRage <molot97@gmail.com>
2026-05-31 15:58:36 +0500

    Lab10.2

--- PascalCompiler/PascalCompiler/LexicalAnalyzer.cs
+++ PascalCompiler/PascalCompiler/LexicalAnalyzer.cs
@@ -32,33 +32,119 @@ namespace PascalCompiler.Lexer
             Advance();
         }
 
-        private void Advance()
+        public void Advance()
+        {
+            if (_hasPeek)
+            {
+                _currentChar = _peekChar;
+                _hasPeek = false;
+            }
+            else
         {
             _currentChar = _io.NextCh();
+        }
+        }
+
+        public char Peek()
+        {
+            if (!_hasPeek)
+            {
+                _peekChar = _io.NextCh();
+                _hasPeek = true;
+            }
+            return _peekChar;
+        }
+
+        public void SkipWhitespaceAndComments()
+        {
+            while (true)
+            {
+                if (char.IsWhiteSpace(_currentChar))
+                {
+                    Advance();
+                }
+                else if (_currentChar == '{')
+                {
+                    int sLine = _io.CurrentLine;
+                    int sCol = _io.CurrentColumn;
+                    Advance();
+                    bool isClosed = false;
+
+                    while (_currentChar != '\0')
+                    {
+                        if (_currentChar == '}')
+                        {
+                            isClosed = true;
+                            Advance();
+                            break;
+                        }
+                        Advance();
+                    }
+                    if (!isClosed)
+                    {
+                        string msg = "Незакрытый комментарий '{'";
+                        _errorTable.AddError(sLine, sCol, msg);
+                    }
+                }
+                else if (_currentChar == '(' && Peek() == '*')
+                {
+                    int sLine = _io.CurrentLine;
+                    int sCol = _io.CurrentColumn;
+                    Advance();
+                    Advance();
+                    bool isClosed = false;
+
+                    while (_currentChar != '\0')
+                    {
+                        if (_currentChar == '*' && Peek() == ')')
+                        {
+                            Advance();
+                            Advance();
+                            isClosed = true;
+                            break;
+                        }
+                        Advance();
+                    }
+                    if (!isClosed)
+                    {
+                        string msg = "Незакрытый комментарий '(*'";
+                        _errorTable.AddError(sLine, sCol, msg);
+                    }
+                }
+                else if (_currentChar == '/' && Peek() == '/')
+        {
+                    Advance();
+                    Advance();
+                    while (_currentChar != '\n' && _currentChar != '\0')
+            {
+                Advance();
+            }
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
 
         public Token GetNextToken()
         {
-            while (char.IsWhiteSpace(_currentChar))
-            {
-                Advance();
-            }
+            SkipWhitespaceAndComments();
 
             if (_io.IsEof || _currentChar == '\0')
             {
-                int eLine = _io.CurrentLine;
-                int eCol = _io.CurrentColumn;
-                return new Token(TokenType.Eof, "EOF", eLine, eCol);
+                return new Token(TokenType.Eof, "EOF",
+                    _io.CurrentLine, _io.CurrentColumn);
             }
 
             int startLine = _io.CurrentLine;
             int startCol = _io.CurrentColumn;
 
-            if (char.IsLetter(_currentChar))
+            if (char.IsLetter(_currentChar) || _currentChar == '_')
             {
                 StringBuilder sb = new StringBuilder();
-
-                while (char.IsLetterOrDigit(_currentChar))
+                while (char.IsLetterOrDigit(_currentChar) ||
+                       _currentChar == '_')
                 {
                     sb.Append(_currentChar);
                     Advance();
@@ -94,6 +180,34 @@ namespace PascalCompiler.Lexer
                     startLine, startCol);
             }
 
+            if (_currentChar == '\'')
+            {
+                Advance();
+                StringBuilder sb = new StringBuilder();
+                bool isClosed = false;
+
+                while (_currentChar != '\0' && _currentChar != '\n')
+                {
+                    if (_currentChar == '\'')
+                    {
+                        isClosed = true;
+                        Advance();
+                        break;
+                    }
+                    sb.Append(_currentChar);
+                    Advance();
+                }
+
+                if (!isClosed)
+                {
+                    string msg = "Незакрытая строковая кавычка '''";
+                    _errorTable.AddError(startLine, startCol, msg);
+                }
+
+                return new Token(TokenType.StringLiteral, sb.ToString(),
+                    startLine, startCol);
+            }
+
             char ch = _currentChar;
             Advance();
 
@@ -114,13 +228,26 @@ namespace PascalCompiler.Lexer
                 case '=':
                     return new Token(TokenType.Equal, "=",
                         startLine, startCol);
-                case '$':
-                    _errorTable.AddError(startLine, startCol,
-                        "Символ '$'");
-                    return new Token(TokenType.Dollar, "$",
+                case '+':
+                    return new Token(TokenType.Plus, "+",
+                        startLine, startCol);
+                case '-':
+                    return new Token(TokenType.Minus, "-",
+                        startLine, startCol);
+                case '*':
+                    return new Token(TokenType.Multiply, "*",
+                        startLine, startCol);
+                case '/':
+                    return new Token(TokenType.Divide, "/",
+                        startLine, startCol);
+                case '(':
+                    return new Token(TokenType.LeftParen, "(",
+                        startLine, startCol);
+                case ')':
+                    return new Token(TokenType.RightParen, ")",
                         startLine, startCol);
                 default:
-                    string msg = $"Неизвестный символ: '{ch}'";
+                    string msg = $"Недопустимый символ: '{ch}'";
                     _errorTable.AddError(startLine, startCol, msg);
                     return new Token(TokenType.Unknown, ch.ToString(),
                         startLine, startCol);
